b6728f500adf5172e0bf6bcbceca9d320ec8fbc8
Andrew Robida <afrobida@gmail.com>
2017-06-17 00:19:43 +0000

    added canvas with min animation

--- game.js
+++ game.js
@@ -1,12 +1,29 @@
 var canvas;
 var canvasContext;
+var ballX=50;
 
 window.onload = function() {
-    console.log("Hiii!!!!");
     
     canvas=document.getElementById('gameCanvas');
     canvasContext=canvas.getContext('2d');
+    
+    var framesPerSecond=30;
+    setInterval(function() {
+        drawEverything();
+        moveEverything();
+    }, 1000/framesPerSecond);
+}
+
+function moveEverything() {
+    ballX+=5;
+}
+
+function drawEverything() {
+    
     canvasContext.fillStyle='black';
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
-    
+    canvasContext.fillStyle='white';
+    canvasContext.fillRect(0,210,10,100);
+    canvasContext.fillStyle='red';
+    canvasContext.fillRect(ballX,100,10,10);   
 }
